c8f279be713e0c69e545122efccb2936bdc6729a
João Victor Dias de Moraes <54956887+jvxdev@users.noreply.github.com>
2021-03-25 13:28:45 -0300

    Início da criação do algoritimo para calculado do frete do produto
    Algoritimo sendo implementado para calcular preço do frete etc

---
+++ LojaVirtual/Libraries/Manager/Shipping/CalculatePackage.cs
@@ -0,0 +1,18 @@
+﻿using LojaVirtual.Models.ProductAggregator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Models;
+
+namespace LojaVirtual.Libraries.Manager.Shipping
+{
+    public class CalculatePackage
+    {
+        public List<Package> CalculateProductsPackage(List<Product> products)
+        {
+            List<Package> packages = new List<Package>();
+            return packages;
+        }
+    }
+}
---
+++ LojaVirtual/Models/Package.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Manager.Shipping
+{
+    public class Package
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Lenght { get; set; }
+        public int Wight { get; set; }
+    }
+}
