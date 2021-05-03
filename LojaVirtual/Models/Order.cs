03e5abe5df5f7176611f97e69317a1f4995ffeba
João Victor Dias de Moraes <54956887+jvxdev@users.noreply.github.com>
2021-05-03 16:09:15 -0300

    Criação da classe de modelo de pedido
    Classe criada e implementada.

---
+++ LojaVirtual/Models/Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+
+        public int? ClientId { get; set; }
+
+
+        public string TransactionId { get; set; }
+
+
+        public string TransactionData { get; set; }
+
+
+        public string FreteCompany { get; set; }
+
+
+        public string FreteTrackingCod { get; set; }
+
+
+        public string PaymentForm { get; set; }
+
+
+        public decimal TotalValue { get; set; }
+
+
+        public string ProductsData { get; set; }
+
+
+        public DateTime RegistryData { get; set; }
+
+
+        public string Situation { get; set; }
+
+
+        public string NFE { get; set; }
+    }
+}
