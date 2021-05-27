51b56255f31441d27c1f884c403a946a7706de61
João Victor Dias de Moraes <54956887+jvxdev@users.noreply.github.com>
2021-05-27 13:32:20 -0300

    Novo controlador e nova view para endereço de entrega
    Futuramente o cliente poderá atualizar seu endereço atual, além de poder criar novos endereços, editá-los e removê-los

---
+++ LojaVirtual/Areas/Client/Controllers/DeliveryAddressController.cs
@@ -0,0 +1,36 @@
+﻿using LojaVirtual.Libraries.Filters;
+using LojaVirtual.Libraries.Login;
+using LojaVirtual.Repositories.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Areas.Client.Controllers
+{
+    [ClientAuthorization]
+    public class DeliveryAddressController : Controller
+    {
+        private ClientLogin _clientLogin;
+        private IDeliveryAddressRepository _deliveryAddressRepository;
+
+
+        public DeliveryAddressController(ClientLogin clientLogin, IDeliveryAddressRepository deliveryAddressRepository)
+        {
+            _clientLogin = clientLogin;
+            _deliveryAddressRepository = deliveryAddressRepository;
+        }
+
+
+        public IActionResult Index()
+        {
+            var client = _clientLogin.GetClient();
+
+            ViewBag.Client = client;
+            ViewBag.DeliveryAddress = _deliveryAddressRepository.ReadAll(client.Id);
+
+            return View();
+        }
+    }
+}
