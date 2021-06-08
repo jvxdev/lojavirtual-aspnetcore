0f28841e25954eb296666093b4b6d4942b563ac3
João Victor Dias de Moraes <54956887+jvxdev@users.noreply.github.com>
2021-06-08 11:00:26 -0300

    Novo Model e ViewComponent para status do pedido
    Novo model e novo ViewComponent implementados.

--- LojaVirtual/Libraries/Component/OrderSituationViewComponent.cs
+++ LojaVirtual/Libraries/Component/OrderSituationViewComponent.cs
@@ -1,11 +1,30 @@
 ﻿using LojaVirtual.Models;
+using LojaVirtual.Models.Const;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LojaVirtual.Libraries.Component
 {
     public class OrderSituationViewComponent : ViewComponent
     {
+        List<OrderSituationStatus> timeline1 {
+            get 
+            {
+                var list = new List<OrderSituationStatus>();
+
+                list.Add(new OrderSituationStatus() { Situation = OrderSituationConst.AGUARDANDO_PAGAMENTO, Concluded = false });
+                list.Add(new OrderSituationStatus() { Situation = OrderSituationConst.PAGAMENTO_APROVADO, Concluded = false });
+                list.Add(new OrderSituationStatus() { Situation = OrderSituationConst.NF_EMITIDA, Concluded = false });
+                list.Add(new OrderSituationStatus() { Situation = OrderSituationConst.EM_TRANSPORTE, Concluded = false });
+                list.Add(new OrderSituationStatus() { Situation = OrderSituationConst.ENTREGUE, Concluded = false });
+                list.Add(new OrderSituationStatus() { Situation = OrderSituationConst.FINALIZADO, Concluded = false });
+                
+                return list;
+            } 
+        }
+
         public OrderSituationViewComponent()
         {
         }
@@ -13,6 +32,27 @@ namespace LojaVirtual.Libraries.Component
 
         public async Task<IViewComponentResult> InvokeAsync(Order order)
         {
+            var listStatusTimeline1 = new List<string>()
+            {
+                OrderSituationConst.AGUARDANDO_PAGAMENTO,
+                OrderSituationConst.PAGAMENTO_APROVADO,
+                OrderSituationConst.NF_EMITIDA,
+                OrderSituationConst.EM_TRANSPORTE,
+                OrderSituationConst.ENTREGUE,
+                OrderSituationConst.FINALIZADO
+            };
+
+            if (listStatusTimeline1.Contains(order.Situation))
+            {
+                foreach (var orderSituation in order.OrderSituations)
+                {
+                    var orderSituationTimeline = timeline1.Where(a => a.Situation == orderSituation.Situation).FirstOrDefault();
+
+                    orderSituationTimeline.Date = orderSituation.Date;
+                    orderSituationTimeline.Concluded = true;
+                }
+            }
+
             return View();
         }
     }
---
+++ LojaVirtual/Models/OrderSituationStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models
+{
+    public class OrderSituationStatus
+    {
+        public string Situation { get; set; }
+
+
+        public DateTime Date { get; set; }
+
+
+        public bool Concluded { get; set; }
+    }
+}
